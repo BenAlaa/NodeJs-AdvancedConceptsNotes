087fbc0a5df9dba44524f80b3556f030e8c83494
BenAlaa <ahmed.alaa0794@gmail.com>
2020-08-25 16:33:57 +0200

    process and threads

---
+++ 01 The Internals of Node/03.process and threads.js
@@ -0,0 +1,9 @@
+// Process: is instance of running computer program
+// inside one process we can have multible threads
+// Thread is a todo list of some instructions that CPU will do
+// OS Schedular: decide which tread will be processed 
+// ergent threads won't take to long time to be processed
+// to improve the proccessing time there are to approachesL
+//      1. to add more cores.
+//      2. to inhance the schedular itself by using threads while execute I/O operations.
+
