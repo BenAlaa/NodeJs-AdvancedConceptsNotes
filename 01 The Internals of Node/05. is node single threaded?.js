74ae6937ce4ac6d6ad8d45d63051ebd5071139d5
BenAlaa <ahmed.alaa0794@gmail.com>
2020-08-25 17:27:17 +0200

    node isn't totally single threaded

---
+++ 01 The Internals of Node/05. is node single threaded?.js
@@ -0,0 +1,5 @@
+// "Node is single threaded" is not completely true
+// Node (Event loop) ---> single threaded
+// Some node module and libraries is not single threaded
+
+// test this with threads.js
